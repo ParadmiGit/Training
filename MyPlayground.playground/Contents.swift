7967ecfd22256a44113af6c4ae46087ac895c535
aneicik.dmitrij@gmail.com <aneicik.dmitrij@gmail.com>
2022-09-20 21:42:20 +0300

    test

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+var hobbs:Double =   3251.6
+private var thousands:Int =  0
+private var hundreds:Int =   0
+private var tens:Int =       0
+private var hours:Int =      0
+private var decimals:Int =   0
+
+thousands = Int(hobbs/1000)
+hundreds = Int((Int(hobbs) - thousands*1000)/100)
+tens = Int((Int(hobbs) - thousands*1000 - hundreds*100)/10 )
+hours = Int( (Int(hobbs) - thousands * 1000 - hundreds * 100 - tens * 10 ))
+
+
